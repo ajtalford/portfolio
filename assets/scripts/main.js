2e94fca326882ee20bbf2517e0c117ddf9c5f0fb
ajtalford <ajtalford@gmail.com>
2019-03-22 14:40:05 -0400

    Updates

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,22 +1,17 @@
 $('.typed').typeIt({
-    speed: 100,
-    lifeLike: true,
-    autoStart: false,
-    loop: true,
-    loopDelay: 0
-  })
-  .tiType('Web Developer')
-  .tiPause(3000)
-  .tiDelete()
-  .tiType('Designer')
-  .tiPause(3000)
-  .tiDelete()
-  .tiType('Coder')
-  .tiPause(3000)
-  .tiDelete()
-  .tiType('DevOps Engineer')
-  .tiPause(3000)
-  .tiDelete()
-  .tiType('Gamer')
-  .tiPause(3000)
-  .tiDelete()
+  speed: 100,
+  lifeLike: true,
+  autoStart: false,
+  loop: true,
+  loopDelay: 0
+})
+.tiType('Web Developer')
+.tiPause(3000)
+.tiDelete()
+.tiType('Designer')
+.tiPause(3000)
+.tiDelete()
+.tiType('DevOps Engineer')
+.tiPause(3000)
+.tiDelete()
+
